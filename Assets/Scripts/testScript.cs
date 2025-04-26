d7a8122a88f2b102754e229cc8ec1687e9fe1596
Kaden C <63522451+Kadition@users.noreply.github.com>
2025-04-26 16:24:04 -0500

    andrew magic

--- Assets/Scripts/testScript.cs
+++ Assets/Scripts/testScript.cs
@@ -36,18 +36,18 @@ public class testScript : MonoBehaviour
         rightHandVector = rightHand.action.ReadValue<Vector3>();
         leftHandVector = leftHand.action.ReadValue<Vector3>();
 
-        rightHandRotation = rightHandQuaternion.action.ReadValue<Quaternion>();
-        leftHandRotation = leftHandQuaternion.action.ReadValue<Quaternion>();
+        // rightHandRotation = rightHandQuaternion.action.ReadValue<Quaternion>();
+        // leftHandRotation = leftHandQuaternion.action.ReadValue<Quaternion>();
 
-        if((leftHandVector.x - stickTransform.position.x) < tolerance && (leftHandVector.y - stickTransform.position.y) < tolerance && (leftHandVector.z - stickTransform.position.z) < tolerance && grabActionLeft.action.ReadValue<bool>())
+        if((leftHandVector.x - stickTransform.position.x) < tolerance && (leftHandVector.y - stickTransform.position.y) < tolerance && (leftHandVector.z - stickTransform.position.z) < tolerance && grabActionLeft.action.ReadValue<float>() > 0.1f)
         {
             stickTransform.position = new Vector3(leftHandVector.x, leftHandVector.y, leftHandVector.z);
-            stickTransform.rotation = leftHandRotation;
+            stickTransform.rotation = leftHandQuaternion.action.ReadValue<Quaternion>();
         }
-        else if((rightHandVector.x - stickTransform.position.x) < tolerance && (rightHandVector.y - stickTransform.position.y) < tolerance && (rightHandVector.z - stickTransform.position.z) < tolerance && grabActionRight.action.ReadValue<bool>())
+        else if((rightHandVector.x - stickTransform.position.x) < tolerance && (rightHandVector.y - stickTransform.position.y) < tolerance && (rightHandVector.z - stickTransform.position.z) < tolerance && grabActionRight.action.ReadValue<float>() > 0.1f)
         {
             stickTransform.position = new Vector3(rightHandVector.x, rightHandVector.y, rightHandVector.z);
-            stickTransform.rotation = rightHandRotation;
+            stickTransform.rotation = rightHandQuaternion.action.ReadValue<Quaternion>();
         }
 
         stickScript.UpdateStick();
